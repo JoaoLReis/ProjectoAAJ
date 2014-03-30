ba9e2a4f2704468bc8a4ca9e9a00f3f364ad3d1b
JoaoLReis <jype_13@hotmail.com>
2014-03-30 16:46:30 +0100

    mais modificaçoes menores

--- PadiProj/Containers/source/Message.cs
+++ PadiProj/Containers/source/Message.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Containers
+namespace Containers.source
 {
-    public class Class1
+    public class Message
     {
     }
 }
--- PadiProj/Server/source/ServerRemote.cs
+++ PadiProj/Server/source/ServerRemote.cs
@@ -1,36 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using Interfaces;
+using Containers.source;
 
 namespace Server.source
 {
     class ServerRemote : MarshalByRefObject, RemoteServerInterface
     {
-        private string clientURL;
+        private Hashtable clientURL;
         private string ownURL;
+        private string masterURL;
 
         private Array padInts;
 
         private int lastCommittedtransID;
 
-        void send() {}
-        void receive() { }
-        void execute() { }
-        void requestTransID() { }
-        void validate() { }
-        void registerClient() { }
-        void registerReplica() { }
-        void commit() { }
-        void write() { }
-        void read() { }
-        void abort() { }
-        void begin() { }
-        void status() { }
-        void fail() { }
-        void freeze() { }
-        void recover() { }
+        private string activeTransaction;
+
+        public ServerRemote()
+        {
+            clientURL = new Hashtable();
+        }
+
+        void send()
+        {
+        //send a message(to be invoked by itself)
+        }
+
+        void receive(Message msg)
+        {
+        //receive a message(to be invoked by others)
+        }
+
+        void execute() 
+        { 
+        //execute active transaction
+        }
+
+        void requestTransID()
+        { 
+        //request to the master a Transaction ID
+        }
+
+        void validate()
+        { 
+        //Start the validating proccess
+        }
+
+        void registerClient() 
+        { 
+        //register a client and its transaction id in the hashtable
+        }
+
+        void registerReplica()
+        { 
+        //to be invoked by a replica
+        }
+
+        void commit()
+        { 
+        //commit a transaction and send result to client
+        }
+
+        void write(int padintID)
+        { 
+        //write a padint
+        }
+
+        PadInt read() 
+        {
+            return null;
+        //read a padint
+        }
+
+        void abort()
+        { 
+        //abort a transaction
+        }
+
+        void begin() 
+        { 
+        //begin a transaction
+        }
+
+        void status() 
+        { 
+        //
+        }
+
+        void fail() 
+        { 
+        //
+        }
+
+        void freeze() 
+        { 
+        //
+        }
+
+        void recover() 
+        { 
+        //
+        }
 
     }
 }
