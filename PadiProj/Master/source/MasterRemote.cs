16724911203cc2f0c3e2d6cc5e313e154b620e6e
ScientX <tori789@gmail.com>
2014-03-31 16:17:43 +0100

    namespace changes

--- PadiProj/Master/source/MasterRemote.cs
+++ PadiProj/Master/source/MasterRemote.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Interfaces;
-using Containers.source;
+using Containers;
 
 namespace Master
 {
--- PadiProj/Server/source/ServerRemote.cs
+++ PadiProj/Server/source/ServerRemote.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using Interfaces;
-using Containers.source;
+using Containers;
 
 namespace Server.source
 {
