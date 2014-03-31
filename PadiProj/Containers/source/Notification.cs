d777f059f00e1289f49a6014987478ee86ae8676
ScientX <tori789@gmail.com>
2014-03-31 16:17:16 +0100

    simple implementation of container classes

--- PadiProj/Containers/source/Notification.cs
+++ PadiProj/Containers/source/Notification.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Containers.source
+namespace Containers
 {
-    class Notification
+    [Serializable]
+    public class Notification
     {
     }
 }
--- PadiProj/Containers/source/PadInt.cs
+++ PadiProj/Containers/source/PadInt.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Containers.source
+namespace Containers
 {
-    class PadInt
+    [Serializable]
+    public class PadInt
     {
+        private int _id;
+        private int _value;
+
+        public PadInt(int id)
+        {
+            _id = id;
+            _value = 0;
+        }
+
+        public PadInt(int id, int value)
+        {
+            _id = id;
+            _value = value;
+        }
+
+        public int getId()
+        {
+            return _id;
+        }
+
+        public int getVal()
+        {
+            return _value;
+        }
     }
 }
