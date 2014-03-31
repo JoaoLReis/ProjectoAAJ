803f94422a36241f64c7f36222eda9f4756b6e2b
ScientX <tori789@gmail.com>
2014-03-31 17:02:23 +0100

    Master now has a hashtable where padint ids are connected to a server URL.

--- PadiProj/Master/source/MasterRemote.cs
+++ PadiProj/Master/source/MasterRemote.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using Interfaces;
@@ -9,5 +9,33 @@ namespace Master
 {
     class MasterRemote : MarshalByRefObject, RemoteMasterInterface
     {
+        private Hashtable _serverPadInts;
+
+        private string _AvailableServer;
+
+        public MasterRemote()
+        {
+            _serverPadInts = new Hashtable();
+        }
+
+        public void regPadint(int id, string server)
+        {
+            _serverPadInts.Add(id, server);
+        }
+
+        public bool exPadint(int id)
+        {
+            return _serverPadInts.ContainsKey(id);
+        }
+
+        public void regServer(string server)
+        {
+            _AvailableServer = server;
+        }
+
+        public void checkAvailability()
+        {
+
+        }
     }
 }
